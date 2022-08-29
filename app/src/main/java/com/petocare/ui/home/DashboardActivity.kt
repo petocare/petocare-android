65edd28fe728e8bdecf0a1f647e0bf6c4d1d26c7
suhassasuke <suhassasuke4090@gmail.com>
2022-08-29 13:15:32 +0530

    changes

--- app/src/main/java/com/petocare/ui/home/DashboardActivity.kt
+++ app/src/main/java/com/petocare/ui/home/DashboardActivity.kt
@@ -2,11 +2,29 @@ package com.petocare.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import com.petocare.R
 
 class DashboardActivity : AppCompatActivity() {
+
+    val viewModel: DashboardViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+        initMvvm()
     }
+
+    private fun initView() {
+
+    }
+
+    private fun initMvvm() {
+        viewModel.getUsers().observe(this, Observer {
+
+        })
+    }
+
 }
